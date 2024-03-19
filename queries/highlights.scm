e4b4be11901c493307460932de5ca27175c7cf24
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2024-03-19 16:04:04 +0100

    only use keywords in the context where they can appear
    fix: #6

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,49 +1,39 @@
 (string_array "," @punctuation.delimiter)
 (string_array ["[" "]"] @punctuation.bracket)
 
-[
-    "ARG"
-    "AS LOCAL"
-    "AS"
-    "BUILD"
-    "CACHE"
-    "CMD"
-    "COPY"
-    "DO"
-    "ENTRYPOINT"
-    "ENV"
-    "EXPOSE"
-    "FROM"
-    "FROM DOCKERFILE"
-    "FUNCTION"
-    "GIT CLONE"
-    "HOST"
-    "IMPORT"
-    "LABEL"
-    "LET"
-    "PROJECT"
-    "RUN"
-    "SAVE ARTIFACT"
-    "SAVE IMAGE"
-    "SET"
-    "USER"
-    "VERSION"
-    "VOLUME"
-    "WITH DOCKER"
-    "WORKDIR"
-] @keyword
+(arg_command "ARG" @keyword)
+(build_command "BUILD" @keyword)
+(cache_command "CACHE" @keyword)
+(cmd_command "CMD" @keyword)
+(copy_command "COPY" @keyword)
+(do_command "DO" @keyword)
+(entrypoint_command "ENTRYPOINT" @keyword)
+(env_command "ENV" @keyword)
+(expose_command "EXPOSE" @keyword)
+(from_command "FROM" @keyword)
+(from_dockerfile_command "FROM DOCKERFILE" @keyword)
+(function_command "FUNCTION" @keyword)
+(git_clone_command "GIT CLONE" @keyword)
+(host_command "HOST" @keyword)
+(import_command "IMPORT" @keyword)
+(label_command "LABEL" @keyword)
+(let_command "LET" @keyword)
+(project_command "PROJECT" @keyword)
+(run_command "RUN" @keyword)
+(save_artifact_command ["SAVE ARTIFACT" "AS LOCAL"] @keyword)
+(save_image_command "SAVE IMAGE" @keyword)
+(set_command "SET" @keyword)
+(user_command "USER" @keyword)
+(version_command "VERSION" @keyword)
+(volume_command "VOLUME" @keyword)
+(with_docker_command "WITH DOCKER" @keyword)
+(workdir_command "WORKDIR" @keyword)
 
-[
-    "ELSE"
-    "ELSE IF"
-    "END"
-    "FINALLY"
-    "FOR"
-    "IF"
-    "IMPORT"
-    "IN"
-    "TRY"
-] @keyword.control
+(for_command ["FOR" "IN" "END"] @keyword.control)
+(if_command ["IF" "ELSE" "ELSE IF" "END"] @keyword.control)
+(import_command ["IMPORT" "AS"] @keyword.control)
+(try_command ["TRY" "FINALLY" "END"] @keyword.control)
+
 
 [
     (comment)
